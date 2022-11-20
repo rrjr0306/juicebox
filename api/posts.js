874875d4610575f7a7666d50123bac171eb49e11
rrjr0306 <rrjr0306@gmail.com>
2022-11-20 01:20:51 -0500

    Finalize POST posts

--- api/posts.js
+++ api/posts.js
@@ -1,14 +1,49 @@
 const express = require('express');
 const postsRouter = express.Router();
 
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
 
-postsRouter.use((req, res, next) => {
-  console.log("A request is being made to /posts");
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    
+    const { title, content, tags = "" } = req.body;
+    
+    const data = req.user
+    const authorId = data.id
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = { authorId, title, content };
+    console.log('this is POST DATAAAAAA', postData)
+
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try {
+        const post = await createPost(postData);
+
+        if (post) {
+            res.send({ post });
+        } else {
+            next({ 
+                name: "CreatePostsError",
+                message: "Error Message Here"
+            });
+        }
+
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+
+
+// postsRouter.use((req, res, next) => {
+//   console.log("A request is being made to /posts");
 
 //   res.send({ message: 'hello from /posts!' });
-    next();
-});
+//     next();
+// });
 
 
 postsRouter.get('/', async (req, res) => {
