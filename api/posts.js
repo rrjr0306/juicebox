679d49878a196ebe2d581f38a5270ae8989e42ac
rrjr0306 <rrjr0306@gmail.com>
2022-11-20 20:07:29 -0500

    Finalize project

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const postsRouter = express.Router();
 
-const { getAllPosts, createPost } = require('../db');
+const { getAllPosts, createPost, updatePost, getPostById } = require('../db');
+const tagsRouter = require('./tags');
 const { requireUser } = require('./utils');
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
@@ -36,7 +37,62 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
     }
 });
 
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
 
+    const updateFields = {};
+
+    if (tags && tags.length > 0) {
+        updateFields.tags = tags.trim().split(/\s+/);
+    }
+
+    if (title) {
+        updateFields.title = title;
+    }
+
+    if (content) {
+        updateFields.content = content;
+    }
+
+    try {
+        const originalPost = await getPostById(postId);
+
+        if (originalPost.author.id === req.user.id) {
+            const updatedPost = await updatePost(postId, updateFields);
+            res.send({ post: updatedPost })
+        } else {
+            next({
+                name: 'UnathorizedUserError',
+                message: 'You cannot update a post that is not yours'
+            })
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+        const post = await getPostById(req.params.postId);
+
+        if (post && post.author.id === req.user.id) {
+            const updatedPost = await updatePost(post.id, { active: false });
+
+            res.send({ post: updatedPost });
+        } else {
+            next(post ? {
+                name: "UnauthorizedUserError",
+                message: "You cannot delete a post which is not yours!"
+            } : {
+                name: "PostNotFoundError",
+                message: "That post does not exist"
+            });
+        }
+    } catch ({ name, message }) {
+        next ({ name, message })
+    }
+});
 
 // postsRouter.use((req, res, next) => {
 //   console.log("A request is being made to /posts");
@@ -47,11 +103,19 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
 
 
 postsRouter.get('/', async (req, res) => {
-    const posts = await getAllPosts();
-    
-    res.send({
+    try {
+        const allPosts = await getAllPosts();
+        
+        const posts = allPosts.filter(post => {
+            return post.active || (req.user && post.author.id === req.user.id);
+          });
+        
+        res.send({
         posts
     });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
 });
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tagsRouter = express.Router();
 
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -10,6 +10,23 @@ tagsRouter.use((req, res, next) => {
     next();
 });
 
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params;
+    const fields = req.body;
+
+    console.log('tagName:', tagName)
+    console.log('fields', fields)
+    try {
+        const tagPosts = await getPostsByTagName(tagName);
+        const posts = tagPosts.filter(post => {
+            return post.active || (req.user && post.author.id === req.user.id);
+          }); 
+        console.log('HERE ARE SOME TAGGED POSTSSSS', posts)
+        res.send({ posts: posts });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
 
 tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
